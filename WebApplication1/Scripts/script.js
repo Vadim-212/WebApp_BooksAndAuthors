e11c039898a84dbe107b64e6d29ef1065be5294c
ГорбуновВ <@406-03>
2019-08-22 20:35:42 +0600

    22.08

--- WebApplication1/Scripts/script.js
+++ WebApplication1/Scripts/script.js
@@ -1,4 +1,20 @@
 ﻿$(document).ready(function()
 {
-    
+    $(`#authors-list-search`).on(`input`,function()
+    {
+        if ($(this).val() == "")
+        {
+            $(`#authorsList`).children().removeAttr(`selected`)
+            $($(`#authorsList`).children()[0]).attr(`selected`, ``)
+        }
+        for (let i = 0; i < $(`#authorsList`).children().length; i++)
+        {
+            if ($($(`#authorsList`).children()[i]).text().toLocaleLowerCase().startsWith($(this).val().toLocaleLowerCase()))
+            {
+                $(`#authorsList`).children().removeAttr(`selected`)
+                $($(`#authorsList`).children()[i]).attr(`selected`, ``)
+            }
+            
+        }
+    })
 })
