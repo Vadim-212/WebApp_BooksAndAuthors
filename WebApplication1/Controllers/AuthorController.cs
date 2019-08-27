16875ef1fb44ac06261f45f1aa74388211a8cead
ГорбуновВ <@406-03>
2019-08-27 20:55:04 +0600

    Add partial view for AuthorController

--- WebApplication1/Controllers/AuthorController.cs
+++ WebApplication1/Controllers/AuthorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Entity;
+using System.Data.SqlClient;
 
 namespace WebApplication1.Controllers
 {
@@ -16,7 +17,28 @@ namespace WebApplication1.Controllers
             using (Library db = new Library())
             {
                 authors = db.Authors.ToList();
-            }
+                //var expensiveBooks = db.Books.OrderByDescending(x => x.Price).Join(db.Authors, b => b.AuthorId, a => a.Id, (a, b) => new Authors { Id = b.Id, FirstName = b.FirstName, LastName = b.LastName }).ToList();
+                var expensiveBooks = db.Books.OrderByDescending(x => x.Price).Select(b => b.AuthorId).Take(5).ToList();
+                //expensiveBooks.ForEach(x => db.Authors.Where(y => y.Id == x));
+                //ViewBag.AuthorsList = expensiveBooks;
+                List<Authors> a = new List<Authors>();
+                //foreach(var item in expensiveBooks)
+                //{
+                //    a.Add(db.Authors.Where(x => x.Id == item).FirstOrDefault());
+                //}
+                expensiveBooks.ForEach(x => { a.Add(db.Authors.Where(y => y.Id == x).FirstOrDefault()); });
+                ViewBag.AuthorsList = a;
+
+                List<HelpClass> o = new List<HelpClass>();
+                //var expensiveBooks2 = db.Database.SqlQuery<object>("SELECT TOP 5 a.Id, SUM(Price) as PricesSum FROM Books b JOIN Authors a ON a.Id = b.AuthorId GROUP BY a.Id ORDER BY PricesSum DESC",new SqlParameter());
+                //expensiveBooks2.ForEach(x => { o.Add(new { Id=x.Id,Name=db.Authors.Where(y=>y.Id==x.Id).FirstOrDefault(),PricesSum=x.PricesSum }) })
+                var eb = (from b in db.Books
+                          join au in db.Authors on b.AuthorId equals au.Id
+                          select new { b.Price, au.Id, }).GroupBy(p => p.Id).ToList();
+                //eb.ForEach(x => { o.Add(new HelpClass() { Id = x.Key,Price=x}) })
+
+                int n = 1;
+             }
             return View(authors);
         }
 
@@ -67,6 +89,11 @@ namespace WebApplication1.Controllers
                 db.SaveChanges();
             }
             return Redirect("~/Author/Index");
+        }
+
+        public ActionResult TopAuthorsPartialView()
+        {
+            return PartialView();
         }
     }
 }
