88daac1ff71d5ae0cd5aa3183e64c73936acd0e0
ГорбуновВ <@406-03>
2019-10-17 19:54:42 +0600

    Update for Azure

---
+++ WebApplication1/Controllers/AzureController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace WebApplication1.Controllers
+{
+    public class AzureController : Controller
+    {
+        static string blobStorage = System.Configuration.ConfigurationManager.AppSettings.Get("BlobStorage");
+        static CloudStorageAccount storageAccount = CloudStorageAccount.Parse(blobStorage);
+
+        static CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+        static CloudBlobContainer blobContainer = blobClient.GetContainerReference("asharina67vadimblobstorage");
+        
+        // GET: Azure
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
